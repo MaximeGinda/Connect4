afabc91ec9d4c07ebca19177cc2a5e9562a7279b
MaximeGinda <maximeginda@outlook.com>
2022-03-15 21:38:05 +0100

    Menu upgrade

--- Connect4/Assets/Script/MainMenu.cs
+++ Connect4/Assets/Script/MainMenu.cs
@@ -1,12 +1,24 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public string GameLevel;
+    public GameObject settingsWindow;
 
-    public void playGame(){
-
+    // Lance le jeu
+    public void playGame()
+    {
+        SceneManager.LoadScene(this.GameLevel);
     }
 
+    // Permet d'ouvrir les settings du jeu 
+    public void openSettings()
+    {
+        settingsWindow.SetActive(true);
+    }
+
+    // Quitte le jeu
     public void quitGame(){
         Application.Quit();
     }
